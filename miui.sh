1cac24b1f1a0257c7fcfa1f61d459676895e1af3
DrRamm <kotsareu@gmail.com>
2017-07-19 22:25:23 +0300

    сделай красиво

--- miui.sh
+++ miui.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+
+DATE_START=$(date +"%s")
 clear
 F=$1
+
+PREFFIX_FOR_ZIP="drramm_"
 
 MIUI_FOLDER=~/miui_unpacked
 GIT_FOLDER=~/miui_cancro_stuff
 TEMP_RECOMPILE_FOLDER=~/miui_recomp_temp
 
 PATCH_FOLDER=$TEMP_RECOMPILE_FOLDER/patches
-TEMP_PATCH_FOLDER=$TEMP_RECOMPILE_FOLDER/shit
+TEMP_PATCH_FOLDER=$TEMP_RECOMPILE_FOLDER/another_temp_folder
 
 STOCK_FOLDER=$GIT_FOLDER/STOCK
 MOD_FOLDER=$GIT_FOLDER/MOD
@@ -27,11 +31,22 @@ if [ ! -f $F ] || [ -z "$F" ]; then
         exit 1 
 fi
 
-DATE_START=$(date +"%s")
+
 
 echo "Очистка временных папок"
 rm -rf $TEMP_RECOMPILE_FOLDER
 rm -rf $MIUI_FOLDER
+
+echo
+echo "Распаковка $F в $MIUI_FOLDER"
+unzip -qq $F -d $MIUI_FOLDER
+
+echo "Удаление звуков UI"
+rm -rf $MIUI_SYSTEM/media/audio/ui
+
+echo "Копирование папки $GIT_FOLDER/system/etc в $MIUI_SYSTEM"
+echo
+cp -r $GIT_FOLDER/system/etc $MIUI_SYSTEM
 
 echo "Получение папок для патчей"
 
@@ -43,10 +58,9 @@ echo "Найдено папок: $COUNT_FOLDERS"
 mkdir -p $TEMP_PATCH_FOLDER
 cd $TEMP_PATCH_FOLDER
 
-echo "Обновление патчей"
 echo
+echo "Обновление патчей на основе разницы STOCK и MOD"
 mkdir -p $PATCH_FOLDER
-
 
 for ((i=1; i <= $COUNT_FOLDERS; i ++))
 	do	
@@ -65,36 +79,25 @@ for ((i=1; i <= $COUNT_FOLDERS; i ++))
 		eval $GIT_RESET
 	done
 
-cd $GIT_FOLDER
-
-echo
-echo "Распаковка $F в $MIUI_FOLDER"
-unzip -qq $F -d $MIUI_FOLDER
-
-echo "Удаление звуков UI"
-rm -rf $MIUI_SYSTEM/media/audio/ui
-
-echo "Копирование папки $GIT_FOLDER/system/etc в $MIUI_SYSTEM"
-echo
-cp -r $GIT_FOLDER/system/etc $MIUI_SYSTEM
-
 cd $PATCH_FOLDER
 COUNT_PATCHES="$(ls -1q | wc -l)"
+
+echo
 echo "Создано патчей: $COUNT_PATCHES "
 
 echo "Наложение патчей..."
 for ((i=1; i <= $COUNT_PATCHES; i++))
 	do
 		CURRENT_PATCH="$(ls | sort -n | head -n $i | tail -n 1)"
+		echo
 		echo "Патч [$i/$COUNT_PATCHES]"
 		if [ "${CURRENT_PATCH: -4}" == "_jar" ]
 		then 
 
 			CURRENT_PATH="$(find $MIUI_SYSTEM -name "${CURRENT_PATCH:: -4}.jar")"
-			#echo $CURRENT_PATH
+			
 			echo "Распаковка $CURRENT_PATCH.jar в $TEMP_RECOMPILE_FOLDER/$CURRENT_PATCH"
 			mkdir -p $TEMP_RECOMPILE_FOLDER
-
 			unzip -qq $CURRENT_PATH -d $TEMP_RECOMPILE_FOLDER/$CURRENT_PATCH
 
 			cd $TEMP_RECOMPILE_FOLDER/$CURRENT_PATCH
@@ -102,7 +105,7 @@ for ((i=1; i <= $COUNT_PATCHES; i++))
 			echo "Распаковка classes.dex"
 			baksmali d classes.dex -o $TEMP_SMALI_FOLDER
 			
-			echo "Применение патча $CURRENT_PATCH с $PATCH_FOLDER"
+			echo "Применение патча $CURRENT_PATCH из $PATCH_FOLDER"
 			eval $GIT_INIT
 			eval $GIT_ADD	
 			git apply $PATCH_FOLDER/$CURRENT_PATCH
@@ -124,11 +127,9 @@ for ((i=1; i <= $COUNT_PATCHES; i++))
 
 		else
 			CURRENT_PATH="$(find $MIUI_SYSTEM -name "$CURRENT_PATCH.apk")"
-			#echo $CURRENT_PATH
 
 			echo "Распаковка $CURRENT_PATCH.apk в $TEMP_RECOMPILE_FOLDER/$CURRENT_PATCH"
 			mkdir -p $TEMP_RECOMPILE_FOLDER
-
 			unzip -qq $CURRENT_PATH -d $TEMP_RECOMPILE_FOLDER/$CURRENT_PATCH
 
 			cd $TEMP_RECOMPILE_FOLDER/$CURRENT_PATCH
@@ -136,7 +137,7 @@ for ((i=1; i <= $COUNT_PATCHES; i++))
 			echo "Распаковка classes.dex"
 			baksmali d classes.dex -o $TEMP_SMALI_FOLDER
 
-			echo "Применение патча $CURRENT_PATCH с $PATCH_FOLDER"
+			echo "Применение патча $CURRENT_PATCH из $PATCH_FOLDER"
 			eval $GIT_INIT
 			eval $GIT_ADD		
 			git apply $PATCH_FOLDER/$CURRENT_PATCH
@@ -155,24 +156,22 @@ for ((i=1; i <= $COUNT_PATCHES; i++))
 			cd $PATCH_FOLDER
 
 			echo "Готово"
-			echo
-		fi
-
+		fi	
 done
 
 echo "Запаковка в финальный архив"
 
 cd $MIUI_FOLDER
-MY="drramm_"
-MY=$MY$F
 
-echo "Архив $MY"
-zip -rqq ../$MY *
+PREFFIX_FOR_ZIP=$PREFFIX_FOR_ZIP$F
 
-cd $GIT_FOLDER
+echo "Архив $PREFFIX_FOR_ZIP"
+zip -rqq ../$PREFFIX_FOR_ZIP *
 
 echo "Готово..."
 
 DATE_END=$(date +"%s")
 DIFF=$(($DATE_END - $DATE_START))
 echo "Время: $(($DIFF / 60)) минут и $(($DIFF % 60)) секунд."
+
+cd $GIT_FOLDER
