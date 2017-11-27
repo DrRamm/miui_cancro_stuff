b792723dabb92124be4f2d7e2c66d4fa7a902ef2
DrRamm <kotsareu@gmail.com>
2017-11-27 13:24:48 +0600

    cancro: system: tune changepowermode

--- system/bin/changepowermode.sh
+++ system/bin/changepowermode.sh
@@ -7,6 +7,16 @@ case "$target" in
     "MI 3W" | "MI 3C")
         case "$powermode" in
             "high")
+		 stop mpdecision
+		 sleep 1
+                 echo 1                                    > /sys/devices/system/cpu/cpu1/online
+                 echo 1                                    > /sys/devices/system/cpu/cpu2/online
+                 echo 1                                    > /sys/devices/system/cpu/cpu3/online
+                 echo 0                                    > /sys/devices/system/cpu/sched_mc_power_savings
+                 echo performance                          > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+                 echo performance                          > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+                 echo performance                          > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+                 echo performance                          > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
                  echo 2265600                              > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
                  echo 2265600                              > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
                  echo 2265600                              > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
@@ -21,28 +31,55 @@ case "$target" in
                  echo 1497600                              > /sys/module/cpu_boost/parameters/input_boost_freq
                  echo 40                                   > /sys/module/cpu_boost/parameters/input_boost_ms
                  echo 255                                  > /sys/class/leds/lcd-backlight/max_brightness
-                 echo 578000000                            > /sys/class/kgsl/kgsl-3d0/max_gpuclk
+                 echo 578000000                            > /sys/class/kgsl/kgsl-3d0/max_gpuclk				 
+		 echo 1                          	   > /sys/class/kgsl/kgsl-3d0/force_bus_on
+		 echo 0                           	   > /sys/class/kgsl/kgsl-3d0/bus_split
+		 echo 1                           	   > /sys/class/kgsl/kgsl-3d0/force_rail_on
+		 echo 1                         	   > /sys/class/kgsl/kgsl-3d0/force_clk_on
+		 echo performance                          > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+		 echo 1000000                              > /sys/class/kgsl/kgsl-3d0/idle_timer
                  echo "msm_cpufreq"                        > $dev_governor
               ;;
             "middle")
+		 stop mpdecision
+		 sleep 1
+                 echo 1                                    > /sys/devices/system/cpu/cpu1/online
+                 echo 1                                    > /sys/devices/system/cpu/cpu2/online
+                 echo 1                                    > /sys/devices/system/cpu/cpu3/online
+                 echo 1                                    > /sys/devices/system/cpu/sched_mc_power_savings
+                 echo interactive                          > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+                 echo interactive                          > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
+                 echo interactive                          > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
+                 echo interactive                          > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
                  echo 2265600                              > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
                  echo 2265600                              > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
                  echo 2265600                              > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
                  echo 2265600                              > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
-                 echo "20000 1400000:40000 1700000:20000"  > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
-                 echo 90                                   > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+                 echo "19000 1400000:35000 1800000:25000"  > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+                 echo 95                                   > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
                  echo 1190400                              > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
-                 echo "85 1500000:90 1800000:70"           > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+                 echo "50  1300000:60 1500000:70 1800000:80 2000000:90"           > /sys/devices/system/cpu/cpufreq/interactive/target_loads
                  echo 40000                                > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
                  echo 20                                   > /sys/module/cpu_boost/parameters/boost_ms
                  echo 1728000                              > /sys/module/cpu_boost/parameters/sync_threshold
                  echo 1190400                              > /sys/module/cpu_boost/parameters/input_boost_freq
                  echo 40                                   > /sys/module/cpu_boost/parameters/input_boost_ms
                  echo 255                                  > /sys/class/leds/lcd-backlight/max_brightness
-                 echo 578000000                            > /sys/class/kgsl/kgsl-3d0/max_gpuclk
+                 echo 462400000                            > /sys/class/kgsl/kgsl-3d0/max_gpuclk
+		 echo 0                        		   > /sys/class/kgsl/kgsl-3d0/force_bus_on
+		 echo 1                        		   > /sys/class/kgsl/kgsl-3d0/bus_split
+		 echo 0                        		   > /sys/class/kgsl/kgsl-3d0/force_rail_on
+		 echo 0                        		   > /sys/class/kgsl/kgsl-3d0/force_clk_on
+		 echo msm-adreno-tz                        > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+		 echo 80                                   > /sys/class/kgsl/kgsl-3d0/idle_timer
                  echo "cpubw_hwmon"                        > $dev_governor
+		 start mpdecision
              ;;
              "low")
+		 stop mpdecision
+		 sleep 1
+		 echo 0                                    > /sys/devices/system/cpu/cpu2/online
+                 echo 0                                    > /sys/devices/system/cpu/cpu3/online
                  echo 1036800                              > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
                  echo 1036800                              > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
                  echo 1036800                              > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
@@ -57,8 +94,15 @@ case "$target" in
                  echo 960000                               > /sys/module/cpu_boost/parameters/input_boost_freq
                  echo 40                                   > /sys/module/cpu_boost/parameters/input_boost_ms
                  echo 100                                  > /sys/class/leds/lcd-backlight/max_brightness
-                 echo 330000000                            > /sys/class/kgsl/kgsl-3d0/max_gpuclk
+                 echo 200000000                            > /sys/class/kgsl/kgsl-3d0/max_gpuclk
+		 echo 0                        		   > /sys/class/kgsl/kgsl-3d0/force_bus_on
+		 echo 1                        		   > /sys/class/kgsl/kgsl-3d0/bus_split
+		 echo 0                        		   > /sys/class/kgsl/kgsl-3d0/force_rail_on
+		 echo 0                        		   > /sys/class/kgsl/kgsl-3d0/force_clk_on
+		 echo msm-adreno-tz                        > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+		 echo 80                                   > /sys/class/kgsl/kgsl-3d0/idle_timer
                  echo "cpubw_hwmon"                        > $dev_governor
+		 start mpdecision
               ;;
         esac
         ;;
